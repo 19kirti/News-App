19c5dc0ea146c263e0f4d74ed77063625f92b8df
19kirti <aggarwalkirti13579@gmail.com>
2024-08-01 13:04:42 +0530

    changed

--- script.js
+++ script.js
@@ -67,7 +67,8 @@ function displayBlocks(articles){
 
         const img = document.createElement("img");
         img.src = article.urlToImage || "https://starryoptics.com/wp-content/uploads/2022/05/NEWS.jpg";
-        img.alt = article.title;
+        img.alt = article.title || "Here's the update on the latest happening!" ;
+        ;
         const heading = document.createElement("h2");
         const shortHead = article.title > 30 ? article.title.slice(0,30)+ "..." : article.title;
         heading.textContent = shortHead; 
